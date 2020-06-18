2bec92a79e39d56ba25480e9707c976c34ec831d
haganmao <kevin_mmc@hotmail.com>
2020-06-18 17:08:34 +1200

    hello world always first

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello React!
+          I am learning you very quickly!!!
         </p>
         <a
           className="App-link"
